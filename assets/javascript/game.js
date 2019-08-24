df4810cabb9b2f227d004ec74d2ffa4efe46a6dc
Chrissy <c.atherholt@gmail.com>
2019-08-24 16:04:43 -0400

    maed some final tweaks that debiugged the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,11 +10,7 @@ var randomWordArray = [
     "greenhats"
 ];
 
-
 // choose a random word from the array above
-
-randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
-console.log(randomWord);
 
 function chooseWord() {
     randomWord = randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
@@ -22,7 +18,6 @@ function chooseWord() {
 }
 
 // some globally defined variables
-var s;
 var count = 12;
 var wins = 0;
 var losses = 0;
@@ -30,18 +25,25 @@ var answerArray = [];
 var guessedArray = [];
 
 function startGame() {
+    chooseWord();
+    answerArray = [];
+    guessedArray = [];
+    count = 12;
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('lettersGuessed').innerHTML = guessedArray;
     for (var i = 0; i < randomWord.length; i++) {
-        // take the random word and replace the userGuesss with underscores
+        // take the random word and replaces the letters with underscores
         answerArray[i] = "_";
     }
     // put the underscores in a string
-    s = answerArray.join(" ");
-    document.getElementById("answer").innerHTML = s;
+    document.getElementById("answer").innerHTML = answerArray.join(" ");;
 }
 
 startGame();
 
 document.onkeyup = function (event) {
+    console.log(answerArray);
     // here we get the letter the user types
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
@@ -63,35 +65,37 @@ document.onkeyup = function (event) {
         document.getElementById("answer").innerHTML = answerArray.join(" ");
     }
 
-    // here's what happens if you LOSE
-    if (count < 1) {
-        losses++;
-        count = 12;
-        guessedArray = [];
-        startGame();
-        chooseWord();
-    }
-
     // here's what happens if you WIN
     if (answerArray.includes("_")) {
 
     } else {
         wins++;
         count = 12;
-        guessedArray = [];
+        document.getElementById('counter').innerHTML = count;
         startGame();
-        chooseWord();
+        return; // keeps the code further down the page from running
+    }
+
+    // here's what happens if you LOSE
+    if (count < 1) {
+        losses++;
+        count = 12;
+        document.getElementById('counter').innerHTML = count;
+        startGame();
+        return;
     }
 
     // here's what happens when you guess letters
+
+    // this makes sure letters arent entered in the array twice
     if (guessedArray.indexOf(userGuess) >= 0) {
 
-    } else {
+    } 
+    // this adds the letters to the guessed letters array
+    else if (count >= 0 ) {
         guessedArray.push(userGuess);
         document.getElementById('lettersGuessed').innerHTML = guessedArray.join(", ");
         console.log(guessedArray);
     }
-    document.getElementById('wins').innerHTML = wins;
-    document.getElementById('losses').innerHTML = losses;
 
 }
